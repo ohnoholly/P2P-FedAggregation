fbef52594c5a636dfe2ba1cb0b17c785f7cf71c4
Holly Wang <hollywang0728@hotmail.com>
2023-04-11 15:17:10 +0200

    Update

---
+++ Core/P2P_Aggregation.py
@@ -0,0 +1,259 @@
+import numpy as np
+import torch
+
+
+class TopKCompressor():
+    """
+    Sparse Communication for Distributed Gradient Descent, Alham Fikri Aji et al., 2017
+    """
+    res = {}
+
+
+    @staticmethod
+    def clear():
+        TopKCompressor.res = {}
+
+
+    @staticmethod
+    def layer_wise_compress(tensor, name, id_node, beta, ratio=0.01):
+        grad = tensor.grad
+        residuals = {}
+
+
+        with torch.no_grad():
+            mask = torch.zeros_like(tensor.data)
+            #rest = torch.ones_like(tensor.data)
+
+            if id_node not in TopKCompressor.res:
+                TopKCompressor.res[id_node] = residuals
+                #TopKCompressor.momen[id_node] = momentum
+                #Check whether it is empty
+            if name not in TopKCompressor.res[id_node]:
+                TopKCompressor.res[id_node][name] = torch.zeros_like(grad.data)
+                #TopKCompressor.momen[id_node][name] = torch.zeros_like(grad.data)
+
+            #Normalize grad
+            grad = torch.abs(grad.data)
+            if grad.dim() == 1:
+                grad = nn.functional.normalize(grad, dim=0)
+            else:
+                grad = nn.functional.normalize(grad)
+
+            #TopKCompressor.momen[id_node][name].data =  beta*TopKCompressor.momen[id_node][name].data + (1-beta)*grad
+
+            TopKCompressor.res[id_node][name].data =  beta*TopKCompressor.res[id_node][name].data + (1-beta)*grad
+
+            # top-k solution
+            # torch.numel returns the total number in the input tensor
+            numel =  TopKCompressor.res[id_node][name].data.numel()
+            k = max(int(numel * ratio), 1)
+            #print("k=",k)
+
+
+            values, indexes = torch.topk(torch.abs(TopKCompressor.res[id_node][name].data).flatten(), k=k)
+            indexes = np.array(np.unravel_index(indexes.cpu().numpy(), TopKCompressor.res[id_node][name].shape)).T
+
+
+            for index in indexes:
+                if len(index) < 2:
+                    mask[index] = 1
+
+                else:
+                    mask[index[0], index[1]] = 1
+
+
+            TopKCompressor.res[id_node][name].data = TopKCompressor.res[id_node][name].data - mask * grad
+
+
+
+            return mask
+
+
+
+    @staticmethod
+    def compress(tensor, id_node, beta, ratio=0.5):
+
+        with torch.no_grad():
+            mask = torch.zeros_like(tensor)
+
+
+            if id_node not in TopKCompressor.res:
+                TopKCompressor.res[id_node] = torch.zeros_like(tensor)
+
+
+            #Normalize grad
+            tensor = torch.abs(tensor)
+            #tensor = nn.functional.normalize(tensor, dim=0)
+
+            TopKCompressor.res[id_node] =  beta*TopKCompressor.res[id_node] + (1-beta)*tensor
+
+            # top-k solution
+            # torch.numel returns the total number in the input tensor
+            numel =  TopKCompressor.res[id_node].numel()
+            k = max(int(numel * ratio), 1)
+            #print("k=",k)
+
+
+            values, indexes = torch.topk(torch.abs(TopKCompressor.res[id_node]).flatten(), k=k)
+            indexes = np.array(np.unravel_index(indexes.cpu().numpy(), TopKCompressor.res[id_node].shape)).T
+
+
+            for index in indexes:
+                mask[index] = 1
+
+
+            TopKCompressor.res[id_node] = TopKCompressor.res[id_node] - mask * tensor
+
+
+            return mask
+
+
+class model_update:
+
+    similarity={}
+    divergence={}
+    params = {}
+
+    def __init__(self):
+        model_update.similarity = {}
+        model_update.divergence = {}
+        model_update.params = {}
+
+
+    @staticmethod
+    def sparse_merge_rand(u_or, u_dest,comb, w, masks_list):
+        params_dest = u_dest.model.named_parameters()
+        dict_params_dest = dict(params_dest)
+        params_or = u_or.model.named_parameters()
+
+        for name1, param1 in params_or:
+            if name1 in dict_params_dest:
+                param2 = dict_params_dest[name1]
+                update = torch.zeros_like(param1.data)
+
+                compressed_o = masks_list[name1] * param1
+                compressed_d = masks_list[name1] * param2
+                update = w*compressed_o.data + comb*compressed_d.data
+
+                with torch.no_grad():
+                    #Pick out the indices which are not zero
+                    indices = torch.nonzero(update)
+                    for index in indices:
+                        index = index.cpu().numpy()
+                        if len(index) < 2:
+                            param2[index] = update[index]
+                        else:
+                            param2[index[0], index[1]] = update[index[0], index[1]]
+
+                    dict_params_dest[name1].data.copy_(param2)
+
+
+    @staticmethod
+    def sparse_merge_top(u_or, u_dest, comb, w, adj_list, compressor, shared=False):
+        grad_o = []
+        para_o = []
+        para_d = []
+
+        if u_dest.id not in model_update.similarity:
+            model_update.similarity[u_dest.id] = {}
+        if u_or.id not in model_update.similarity[u_dest.id]:
+            model_update.similarity[u_dest.id][u_or.id] = []
+
+        if u_dest.id not in model_update.divergence:
+            model_update.divergence[u_dest.id] = {}
+        if u_or.id not in model_update.divergence[u_dest.id]:
+            model_update.divergence[u_dest.id][u_or.id] = []
+
+        if u_dest.id not in model_update.params:
+            model_update.params[u_dest.id] = {}
+        if u_or.id not in model_update.params[u_dest.id]:
+            model_update.params[u_dest.id][u_or.id] = {}
+
+        params_dest = u_dest.model.named_parameters()
+        dict_params_dest = dict(params_dest)
+        params_or = u_or.model.named_parameters()
+        dict_params_or = dict(params_or)
+
+
+        with torch.no_grad():
+            for name1, param1 in dict_params_or.items():
+                if name1 in dict_params_dest:
+                    if name1 != "out.weight":
+                        if name1 !="out.bias":
+                            param2 = dict_params_dest[name1]
+                            grad = torch.flatten(param1.grad)
+                            grad_o.append(grad)
+                            param1 = torch.flatten(param1)
+                            param2 = torch.flatten(param2)
+                            para_o.append(param1)
+                            para_d.append(param2)
+
+        g_o = torch.cat(grad_o, 0)
+        p_o = torch.cat(para_o, 0)
+        p_d = torch.cat(para_d, 0)
+        sim = F.cosine_similarity(p_o, p_d, dim=0)
+        div = torch.norm(p_o-p_d)/torch.norm(p_d)
+        model_update.similarity[u_dest.id][u_or.id].append(sim.item())
+        model_update.divergence[u_dest.id][u_or.id].append(div.item())
+        mask = compressor.compress(g_o, u_or.id, 0.95)
+
+
+        with torch.no_grad():
+            start_share_time = time.time()
+            it = 0
+            for name, param1 in dict_params_or.items():
+                if name in dict_params_dest:
+                    if name != "out.weight":
+                        if name !="out.bias":
+                            param2 = dict_params_dest[name]
+                            num = torch.numel(param1)
+                            size = param1.cpu().numpy().shape
+                            mask_layer = mask[it:it+num]
+                            mask_layer = torch.reshape(mask_layer, size)
+                            it = it + num
+                            compressed_o = mask_layer * param1
+                            compressed_d = mask_layer * param2
+
+                            model_update.params[u_dest.id][u_or.id][name] = compressed_o
+                            update = w*compressed_o.data + comb*compressed_d.data
+
+                            #Pick out the indices which are not zero
+                            indices = torch.nonzero(update)
+                            for index in indices:
+                                index = index.cpu().numpy()
+                                if len(index) < 2:
+                                    param2[index] = update[index]
+                                else:
+                                    param2[index[0], index[1]] = update[index[0], index[1]]
+
+                            dict_params_dest[name].data.copy_(param2)
+
+            end_share_time = time.time()
+            share_time = end_share_time - start_share_time
+            print("share_time:", share_time)
+        
+        if u_or.adv_flag==False and len(u_or.xsyn)!=0 and u_or.share < sum(adj_list[u_or.id-1]) and shared==True:
+            r = torch.randperm(math.floor(u_dest.nos/(sum(adj_list[u_or.id-1]))))
+            o_xsample = u_or.xsyn[r]
+            o_ysample = u_or.ysyn[r]
+
+            if len(u_dest.o_xsyn) == 0:
+                u_dest.o_xsyn = o_xsample
+                u_dest.o_ysyn = o_ysample
+            else:
+                u_dest.o_xsyn = torch.cat((u_dest.o_xsyn, o_xsample), dim=0)
+                u_dest.o_ysyn = torch.cat((u_dest.o_ysyn, o_ysample), dim=0)
+
+            u_or.share = u_or.share + 1
+
+
+    @staticmethod
+    def merge_models_global(m_or,m_dest,alpha_or,alpha_dest):
+
+        params_dest = m_dest.named_parameters()
+        dict_params_dest = dict(params_dest)
+        params_or = m_or.named_parameters()
+        for name1, param1 in params_or:
+            if name1 in dict_params_dest:
+                dict_params_dest[name1].data.copy_(alpha_or*param1.data +
+                            alpha_dest*dict_params_dest[name1].data)
--- Utils/train.py
+++ Utils/train.py
@@ -15,28 +15,40 @@ import torch.optim as optim
 
 # CLASS TO MODEL THE USERS IN THE FEDERATED LEARNING TASK
 class user:
-    def __init__(self,x,y,p_i,idx, model):
+    def __init__(self,x,y,p_i,idx):
         self.xtr = x #Training data
         self.ytr = y #Labels
         self.p = p_i #Contribution to the overall model
         self.n = x.size()[0] #Number of training points provided
         self.id = idx #ID for the user
         self.v = 0.0 # the data variance on user
+        self.ne_ds = 0 # The sum of data samples from all neighbors
+        self.ne_dv = 0. #The sum of the data variance from all neighbors
+        self.ne_sim = [] #The sum of the model similarity from all neighbors in different epochs
+        self.ne_div = [] #The sum of the model divergence from all neighbors in different epochs
+        self.deg = 0. #The degree of the node
+        self.xtst =[] #Test data
+        self.ytst =[] #Test label
+        self.ne_weight = []
+        self.ne_datasize = []
+        self.ne_var = []
+        self.ne_deg = []
+        self.ne_params = {}
+        self.model = Classifier_nonIID()
+        self.tune_model = Classifier_nonIID()
+        self.opt = []
+        self.tune_opt = []
+        self.loss = []
+        self.tune_loss = []
+        self.p_epoch = []
+        self.poison_flag = False # The flag to indicate whether it is model posion
+        self.adv_flag = False #The flag to indicate whether it is adversary
         self.share = 0 # the number of users this user has been shared
         self.nos = 0 # Number for sample from other clients
-        self.model = model
-
-
-    xtst =[] #Test data
-    ytst =[] #Test label
-    opt = []
-    loss = []
-    sim = []
-    p_epoch = []
-    xsyn = []
-    ysyn = []
-    o_xsyn = []
-    o_ysyn = []
+        self.xsyn =[]
+        self.ysyn =[]
+        self.o_xsyn = []
+        self.o_ysyn = []
 
 
 # FUNCTION TO TRAIN THE CLASSIIFER
@@ -530,3 +542,356 @@ def trainFA_imbalanced(the_model, training_data, training_labels, mode, lam, g_e
                     print("merge model", u.id)
                     merge_models_ptp(j, u, mode, nusers, comb)
                     comb = 1.0
+
+
+
+def trainP2P(training_data, training_labels, lam, adj_list, iid=True, test_data='', test_labels='', gdata='', glabel=''):
+
+    nusers = len(training_labels)
+    p0 = 1/nusers
+
+    #TRAINING PARAMETERS (TO BE CHANGED: THIS SHOULD BE GIVEN AS A PARAMETER)
+    epochs = 100 #TOTAL NUMBER OF TRAINING ROUNDS
+    partial_epochs = 5 #NUMBER OF EPOCHS RUN IN EACH CLIENT BEFORE SENDING BACK THE MODEL UPDATE
+    tune_epochs = 1
+    batch_size = 128 #BATCH SIZE
+    ad_array = np.array(adj_list)
+    conn = np.sum(ad_array, 1)
+    deg = np.sum(ad_array)
+
+    errorIterations = []
+
+    print("Creating users...")
+    users = []
+    #TopK-Sparsification
+    compressor = TopKCompressor()
+    updater = model_update()
+
+    #Number of training points from class 0 for every user
+    n0users = torch.zeros(nusers)
+    #Number of training points from class 1 for every user
+    n1users = torch.zeros(nusers)
+
+    for i in range(nusers):
+        users.append(user(training_data[i],training_labels[i],p0,i+1))
+        n1users[i] = training_labels[i].sum()
+        n0users[i] = training_labels[i].size(0) - training_labels[i].sum()
+        users[i].deg = conn[i]/deg
+        if iid == True:
+            users[i].xtst = test_data[i]
+            users[i].ytst = test_labels[i]
+
+
+    #Total number of training samples from class 0 and 1
+    N0 = n0users.sum().numpy()
+    N1 = n1users.sum().numpy()
+
+
+    for u in users:
+        ntr = 0 #number of all connected training set
+        v_sum = 0
+        ntr += u.xtr.size(0)
+        v = getvariance(u.xtr)
+        u.v = torch.norm(v, float('inf'))
+        v_sum += u.v
+        ids = 0
+        for ad in adj_list[u.id-1]:
+            if ad == 1:
+                ntr += users[ids].xtr.size(0)
+                v = getvariance(users[ids].xtr)
+                users[ids].v = torch.norm(v, float('inf'))
+                v_sum += users[ids].v
+
+            ids = ids+1
+
+        u.ne_ds = ntr
+        u.ne_dv = v_sum
+
+
+
+
+    #CREATE MODEL
+    model = Classifier_nonIID().to(device)
+    errorsEpochs = torch.zeros(epochs)
+
+    # Weight the value of the update of each user according to the number of training data points
+    # Assign model to each user
+    u_p = []
+    for u in users:
+        ne_weight = []
+        ids = 0
+        for ad in adj_list[u.id-1]:
+            if ad == 1:
+                datasize = users[ids].xtr.size(0)/u.ne_ds
+                datavar = (users[ids].v/u.ne_dv)
+                #FedAvg
+                p = 1/sum(adj_list[u.id-1],1)
+                #p = lam*datavar + 0.5*(1-lam)*datasize + 0.5*(1-lam)*users[ids].deg
+                print("Weight for user ", users[ids].id, ": ", np.round(p,3))
+                ne_weight.append(p)
+                u.ne_datasize.append(datasize)
+                u.ne_var.append(datavar)
+                u.ne_deg.append(users[ids].deg)
+            else:
+                if ids == u.id-1:
+                    datasize = u.xtr.size(0)/u.ne_ds
+                    datavar = (u.v/u.ne_dv)
+                    #FedAvg
+                    p = 1/sum(adj_list[u.id-1],1)
+                    #p = lam*datavar + 0.5*(1-lam)*datasize + 0.5*(1-lam)*u.deg
+                    print("Weight for user ", u.id, ": ", np.round(p,3))
+                    ne_weight.append(p)
+                    u.ne_datasize.append(datasize)
+                    u.ne_var.append(datavar)
+                    u.ne_deg.append(u.deg)
+                else:
+                    ne_weight.append(0)
+                    u.ne_datasize.append(0.)
+                    u.ne_var.append(0.)
+                    u.ne_deg.append(0.)
+            ids = ids+1
+
+        u_p.append(ne_weight)
+        u.ne_weignt = ne_weight
+        u.model = copy.deepcopy(model).to(device)
+        u.tune_model = copy.deepcopy(model).to(device)
+        #u.opt = optim.SGD(u.model.parameters(), lr=0.00001, momentum=0.9, weight_decay=10)
+        u.opt = optim.Adam(u.model.parameters(), lr=0.0001)
+        u.tune_opt = optim.Adam(u.model.parameters(), lr=0.0001)
+        u.loss = nn.BCELoss()
+        u.tune_loss = nn.BCELoss()
+
+    var = []
+    for u in users:
+        var.append(u.v.item())
+
+
+    sort_var = sorted(var, key=float, reverse=True)
+
+    max_id = var.index(sort_var[0])
+    second = var.index(sort_var[1])
+    third = var.index(sort_var[2])
+    print(max_id)
+    print(second)
+    print(third)
+
+
+    pre_e = 1
+    for e in range(epochs):
+        print("Epoch... ",e)
+        epoch_i_array.append(e)
+
+
+        #Share model with the users
+        for u in range(len(users)):
+            print(users[u].adv_flag)
+            start = time.time()
+            error, pred = train_user(users[u].model,users[u].opt,users[u].loss,users[u].xtr,users[u].ytr,partial_epochs,batch_size, users[u].poison_flag)
+            end = time.time()
+            local_training_time = end-start
+            print("training_time:", local_training_time)
+            print("Loss:", error)
+            errlist[u].append(error)
+
+        """
+        #Share model with the users
+        for u in range(len(users)):
+            print(users[u].adv_flag)
+            error, pred = train_user_FarK(users[u],partial_epochs,batch_size,0.7, 15, e, nusers)
+            print("Loss:", error)
+            errlist[u].append(error)
+        """
+
+
+        #Compute test accuracy
+
+        if iid == True:
+            auc = 0.0
+            for u in range(len(users)):
+                print("User:", u)
+                auc_u, cl_error, fpr, fnr, par, rap, fs = computeTestErrors(users[u].model,users[u].xtst,users[u].ytst)
+                auclist[u].append(auc_u)
+                valerrlist[u].append(cl_error)
+                fslist[u].append(fs)
+                fplist[u].append(fpr)
+                fnlist[u].append(fnr)
+                parlist[u].append(par)
+                raplist[u].append(rap)
+
+
+        else:
+            for u in range(len(users)):
+                print("User:", u)
+                auc_u, cl_error, fpr, fnr, par, rap, fs = computeTestErrors(users[u].model, gdata, glabel)
+                auclist[u].append(auc_u)
+                valerrlist[u].append(cl_error)
+                fslist[u].append(fs)
+                fplist[u].append(fpr)
+                fnlist[u].append(fnr)
+                parlist[u].append(par)
+                raplist[u].append(rap)
+
+        """
+        Create the mask for random-K sparsification.
+        """
+        #Create the mask for sparsification (rand and topk)
+        masks_dict = {}
+        for name, param in model.named_parameters():
+            size = list(param.shape)
+            mask = masks(size, 50).to(device)
+            masks_dict[name] = mask
+
+
+        """
+        Launch the attacks
+        """
+        #Launch attack in epoch 50
+
+
+
+        #if e == 0:
+            #Label flipping attack
+            #fliped_a = label_flipping(users[max_id], 0.95)
+            #fliped_b = label_flipping(users[second], 0.95)
+            #fliped_c = label_flipping(users[third], 0.95)
+            #users[max_id].ytr.copy_(fliped_a)
+            #users[second].ytr.copy_(fliped_b)
+            #users[third].ytr.copy_(fliped_c)
+
+            #Noise attack
+            #noised_xtr, noised_ytr = adding_noise(users[max_id], 2)
+            #noised_xsec, noised_ysec = adding_noise(users[second], 2)
+            #noised_xthi, noised_ythi = adding_noise(users[third], 2)
+
+            #users[max_id].xtr = noised_xtr
+            #users[max_id].ytr = noised_ytr
+            #users[second].xtr = noised_xsec
+            #users[second].ytr = noised_ysec
+            #users[third].xtr = noised_xthi
+            #users[third].ytr = noised_ythi
+
+            #Model poisoning
+            #users[max_id].poison_flag = True
+            #users[second].poison_flag = True
+            #users[third].poison_flag = True
+
+            #users[max_id].adv_flag = True
+            #users[second].adv_flag = True
+            #users[third].adv_flag = True
+
+        #Byazantine attack
+        if e>=0:
+            parameter_poison(users[max_id])
+            #parameter_poison(users[second])
+            #parameter_poison(users[third])
+            if users[max_id].adv_flag == False:
+                users[max_id].adv_flag = True
+                #users[second].adv_flag = True
+                #users[third].adv_flag = True
+
+
+        shared = False
+        """
+        Model aggregation and updating
+        """
+
+        merge_times = []
+        for u in range(len(users)):
+            weights = u_p[u]
+            print(weights)
+            comb = weights[u] # the weight for the current node
+
+            sim_sum = 0
+            div_sum = 0
+            ids = 0
+
+            for ad in adj_list[u]:
+                if ad == 1:
+                    print("merge model", users[ids].id)
+                    updater.sparse_merge_top(users[ids], users[u], comb, weights[ids], adj_list, compressor, shared)
+
+                    sim_sum = sim_sum + updater.similarity[users[u].id][users[ids].id][e]
+                    div_sum = div_sum + updater.divergence[users[u].id][users[ids].id][e]
+                    comb = 1.0
+
+                ids = ids + 1
+            users[u].ne_params = updater.params[users[u].id]
+            end_merge_time = time.time()
+            users[u].ne_sim.append(sim_sum+1)  #+1 is for the similarity to its own model
+            users[u].ne_div.append(div_sum)
+
+        """
+        Baseline: Trimmed mean
+        """
+        #trimmed_mean(users, adj_list, compressor, shared)
+        """
+        Baseline: Median
+        """
+        #get_median(users, adj_list, compressor, shared)
+
+        """
+        Baseline: Krum
+        """
+        #krum(users, adj_list, compressor, shared, 1)
+
+
+
+        """
+        Tunning of the weight of node
+        """
+
+        if e == 1 or (e%10==0 and e!=0):
+            start_tune_time = time.time()
+            for u in users:
+                if u.adv_flag == False:
+                    ww, penal = Bayes_Optimizer(u, adj_list, updater, tune_epochs, batch_size, e)
+                    print("ww:",ww)
+                    print("Penal:",penal)
+
+                    penalty = 0.
+                    for ids, ad in enumerate(adj_list[u.id-1]):
+                        #If the node is adjecent
+                        if ad == 1:
+                            #pens = penal *(updater.divergence[u.id][ids+1][e]/u.ne_div[e])
+                            #score = ww[0]*u.ne_datasize[ids]+ww[1]*u.ne_var[ids]+ww[2]*u.ne_deg[ids]+ww[3]*(updater.similarity[u.id][ids+1][e]/u.ne_sim[e])-pens
+                            #score = ww[0]*u.ne_datasize[ids]+ww[1]*u.ne_var[ids]+ww[2]*u.ne_deg[ids]+ww[3]*(updater.similarity[u.id][ids+1][e]/u.ne_sim[e])
+                            score = ww[0]*u.ne_datasize[ids]+ww[1]*u.ne_var[ids]+ww[2]*u.ne_deg[ids]
+                            #print("pens:",pens)
+                            print("Weight for user ", users[ids].id, ": ", np.round(score,3))
+                            u_p[u.id-1][ids] = score
+                            #penalty = penalty + pens
+
+
+                    #u_weight = ww[0]*u.ne_datasize[u.id-1]+ww[1]*u.ne_var[u.id-1]+ww[2]*u.ne_deg[u.id-1]+ww[3]*(1/u.ne_sim[e])+penalty
+                    #u_weight = ww[0]*u.ne_datasize[u.id-1]+ww[1]*u.ne_var[u.id-1]+ww[2]*u.ne_deg[u.id-1]+ww[3]*(1/u.ne_sim[e])
+                    u_weight = ww[0]*u.ne_datasize[u.id-1]+ww[1]*u.ne_var[u.id-1]+ww[2]*u.ne_deg[u.id-1]
+                    print("Weight for user ", u.id, ": ", np.round(u_weight,3))
+                    u_p[u.id-1][u.id-1] = u_weight
+                    u_p[u.id-1] = [0 if i < 0 else i for i in u_p[u.id-1]]
+                    print(u_p[u.id-1])
+                    num_p = sum(i > 0 for i in u_p[u.id-1])
+                    print(num_p)
+                    thres = sum(u_p[u.id-1])/num_p
+                    print(thres)
+                    if u_p[u.id-1][u.id-1]>thres:
+                        add = u_p[u.id-1][u.id-1]-thres
+                        add = add/(num_p-1)
+                        print(add)
+                        u_p[u.id-1][u.id-1] = thres
+                        for it, i in enumerate(u_p[u.id-1]):
+                            if i!=0:
+                                if it!=u.id-1:
+                                    u_p[u.id-1][it] = u_p[u.id-1][it]+add
+
+                    print(u_p[u.id-1])
+                    u_p[u.id-1] = [float(i)/sum(u_p[u.id-1]) for i in u_p[u.id-1]]
+                    print(u_p[u.id-1])
+
+            end_tune_time = time.time()
+            tune_time = end_tune_time - start_tune_time
+            print("Tune_time:", tune_time)
+
+            pre_e = e
+
+
+    return errorsEpochs
